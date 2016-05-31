e56614f6cc6b8e6ff52422b981ecb2e68319f443
Antony Donovan <antony.donovan@generalassemb.ly>
2016-05-31 11:45:03 -0400

    Add toggling list detail
    Show or hide the list in a listr

--- app/components/listr-list/component.js
+++ app/components/listr-list/component.js
@@ -3,4 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: ['listDetailHidden'],
   listDetailHidden: false,
+  actions: {
+    toggleListDetail () {
+      return this.toggleProperty('listDetailHidden');
+    },
+  },
 });
